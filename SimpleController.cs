9f44a1b1d877ad6ceb10bf9d03f573b3f5d0eb98
tamra9991 <45219184+tamra9991@users.noreply.github.com>
2018-11-20 21:48:02 -0500

    Add files via upload

---
+++ SimpleController.cs
@@ -0,0 +1,55 @@
+﻿/* ===========================================
+ * SimpleController 
+ * 	Attach to a game object.
+ * 	Use WASD to move it around.
+ * 	Use space bar to make it jump.
+=========================================== */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleController : MonoBehaviour {
+
+	// The force multiplier for force on the X and Z axis.
+	public float moveSpeed = 10f;
+
+	// The force multiplier for force on the Y axis.
+	public float jumpSpeed = 200f;
+
+	// The hight (from 0) above which jumping is disabled.
+	public float jumpMinHeight = 0.7f;
+
+	// A reference to the Rigidbody component attached to this game object
+	private Rigidbody myRigidbody;
+
+	void Start () {
+		// Set the reference to my Rigidbody
+		myRigidbody = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	void Update () {
+		// Move up
+		if(Input.GetKey(KeyCode.W)){
+			myRigidbody.AddForce (0, 0, moveSpeed);
+		}
+		// Move left
+		if(Input.GetKey(KeyCode.A)){
+			myRigidbody.AddForce (-moveSpeed, 0, 0);
+		}
+		// Move down
+		if(Input.GetKey(KeyCode.S)){
+			myRigidbody.AddForce (0, 0, -moveSpeed);
+		}
+		// move right
+		if(Input.GetKey(KeyCode.D)){
+			myRigidbody.AddForce (moveSpeed, 0, 0);
+		}
+		// jump
+		if(Input.GetKeyDown(KeyCode.Space)){
+			if (gameObject.transform.position.y < jumpMinHeight) {
+				myRigidbody.AddForce (0, jumpSpeed, 0);
+			}
+		}
+	}
+}
